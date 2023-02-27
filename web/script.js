8942e6634d1f6b8889e566a37363e05b27ea8cb8
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2023-02-27 14:13:16 +0100

    set consistent download attribute for all exports

--- web/script.js
+++ web/script.js
@@ -1,4 +1,6 @@
 var sessionId;
+var curGeojson;
+var curGeojsonId = -1;
 
 var urlParams = new URLSearchParams(window.location.search);
 var qleverBackend = urlParams.get("backend");
@@ -12,29 +14,18 @@ map.attributionControl.setPrefix('University of Freiburg');
 
 var layerControl = L.control.layers([], [], {collapsed:true, position: 'topleft'}).addTo(map);
 
-var curGeojson;
-var curGeojsonId = -1;
-
 var osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a rel="noreferrer" target="_blank" href="#">OpenStreetMap</a>',
     maxZoom: 19,
     opacity:0.9
 }).addTo(map);
 
-function trimStr(str) {
-    if (str.length > 100) {
-        return str.substring(0, 100) + " [...]";
-    }
-
-    return str;
-}
-
 function openPopup(data) {
     if (data.length > 0) {
         select_variables = [];
         row = [];
 
-        for (var i in data[0]["attrs"]) {
+        for (let i in data[0]["attrs"]) {
             select_variables.push(data[0]["attrs"][i][0]);
             row.push(data[0]["attrs"][i][1]);
         }
@@ -50,13 +41,13 @@ function openPopup(data) {
         // "_image", then show an image with that URL in the first column of the
         // table. Note that we compute the cell contents here and add it during the
         // loop (it has to be the first cell of a table row).
-        var image_cell = "";
+        let image_cell = "";
         if (select_variables.length >= 2) {
-          var image_column = select_variables.length - 2;
+          let image_column = select_variables.length - 2;
           if (select_variables[image_column] == "?image" ||
                 select_variables[image_column] == "?flag" ||
                 select_variables[image_column].endsWith("_image")) {
-            var num_table_rows = select_variables.length - 2;
+            let num_table_rows = select_variables.length - 2;
             image_url = row[image_column];
             if (image_url != null)
               image_cell = "<td rowspan=\"" + num_table_rows + "\"><a target=\"_blank\" href=\"" + image_url.replace(/^[<"]/, "").replace(/[>"]$/, "") + "\"><img src=\""
@@ -66,7 +57,7 @@ function openPopup(data) {
         }
 
         // Now compute the table rows in an array.
-        var popup_content_strings = [];
+        let popup_content_strings = [];
         select_variables.forEach(function(variable, i) {
           // Skip the last column (WKT literal) and the ?image column (if it
           // exists).
@@ -113,7 +104,7 @@ function openPopup(data) {
 }
 
 function getGeoJsonLayer(geom) {
-	var color = "#e6930e";
+	const color = "#e6930e";
     return L.geoJSON(geom, {
 			style: {color : color, fillColor: color, weight: 7, fillOpacity: 0.2},
             pointToLayer: function (feature, latlng) {
@@ -141,13 +132,13 @@ function showError(error) {
 function loadMap(id, bounds) {
     console.log("Loading session " + id);
     document.getElementById("msg").style.display = "none";
-    var ll = L.Projection.SphericalMercator.unproject({"x": bounds[0][0], "y":bounds[0][1]});
-    var ur =  L.Projection.SphericalMercator.unproject({"x": bounds[1][0], "y":bounds[1][1]});
-    var bounds = [[ll.lat, ll.lng], [ur.lat, ur.lng]];
-    map.fitBounds(bounds);
+    const ll = L.Projection.SphericalMercator.unproject({"x": bounds[0][0], "y":bounds[0][1]});
+    const ur =  L.Projection.SphericalMercator.unproject({"x": bounds[1][0], "y":bounds[1][1]});
+    const boundsLatLng = [[ll.lat, ll.lng], [ur.lat, ur.lng]];
+    map.fitBounds(boundsLatLng);
     sessionId = id;
 
-	var heatmapLayer = L.nonTiledLayer.wms('heatmap', {
+	const heatmapLayer = L.nonTiledLayer.wms('heatmap', {
         minZoom: 0,
         maxZoom: 19,
         opacity: 0.8,
@@ -157,7 +148,7 @@ function loadMap(id, bounds) {
         transparent: true,
     });
 
-	var objectsLayer = L.nonTiledLayer.wms('heatmap', {
+	const objectsLayer = L.nonTiledLayer.wms('heatmap', {
         minZoom: 0,
         maxZoom: 19,
         opacity: 0.8,
@@ -175,8 +166,8 @@ function loadMap(id, bounds) {
 	layerControl.addBaseLayer(objectsLayer, "Objects");
 
     map.on('click', function(e) {
-        var ll= e.latlng;
-        var pos = L.Projection.SphericalMercator.project(ll);
+        const ll= e.latlng;
+        const pos = L.Projection.SphericalMercator.project(ll);
 
         fetch('pos?x=' + pos.x + "&y=" + pos.y + "&id=" + id + "&rad=" + (100 * Math.pow(2, 14 - map.getZoom())))
           .then(response => response.json())
@@ -199,9 +190,7 @@ function loadMap(id, bounds) {
 console.log("Loading data from QLever...");
 fetch('query' + window.location.search)
   .then(response => {
-        if (!response.ok) {
-            return response.text().then(text => {throw new Error(text)});
-        }
+      if (!response.ok) return response.text().then(text => {throw new Error(text)});
       return response;
     })
   .then(response => response.json())
@@ -209,13 +198,24 @@ fetch('query' + window.location.search)
   .catch(error => {showError(error);});
 
 document.getElementById("export-geojson").onclick = function() {
-    if (sessionId) window.location.href = "/export?id="+ sessionId;
+    if (sessionId) {
+        let a = document.createElement("a");
+        a.href = "export?id="+ sessionId;
+        a.setAttribute("download", "export.json");
+        a.click();
+    }
 }
 
 document.getElementById("export-tsv").onclick = function() {
-	window.location.href = qleverBackend + "?query=" + encodeURIComponent(query) + "&action=tsv_export";
+    let a = document.createElement("a");
+    a.href = qleverBackend + "?query=" + encodeURIComponent(query) + "&action=tsv_export";
+    a.setAttribute("download", "export.tsv");
+    a.click();
 }
 
 document.getElementById("export-csv").onclick = function() {
-	window.location.href = qleverBackend + "?query=" + encodeURIComponent(query) + "&action=csv_export";
+    let a = document.createElement("a");
+    a.href = qleverBackend + "?query=" + encodeURIComponent(query) + "&action=csv_export";
+    a.setAttribute("download", "export.csv");
+    a.click();
 }
